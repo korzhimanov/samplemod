c009a7089373e8be628daecb3bb0e3533344bcd8
Artem Korzhimanov <korzhimanov.artem@gmail.com>
2017-01-30 20:09:24 +0300

    Changed default Author and e-mail in the sample

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ setup(
     version='0.1.0',
     description='Sample package for Python-Guide.org',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Artem Korzhimanov',
+    author_email='korzhimanov.artem@gmail.com',
+    url='https://github.com/korzhimanov/samplemod',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
